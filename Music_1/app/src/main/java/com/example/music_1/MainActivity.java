eff197c27da3fedc93df1851ff8cabc1a68cf14e
LuongDinhHoang <61779034+LuongDinhHoang@users.noreply.github.com>
2020-09-28 11:32:19 +0700

    dd

--- Music_1/app/src/main/java/com/example/music_1/MainActivity.java
+++ Music_1/app/src/main/java/com/example/music_1/MainActivity.java
@@ -91,14 +91,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
         //////////////navigation
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close);
-        if (drawer != null) {
-            drawer.addDrawerListener(toggle);
-        }
-        toggle.syncState();
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open,
+//                R.string.navigation_drawer_close);
+//        if (drawer != null) {
+//            drawer.addDrawerListener(toggle);
+//        }
+//        toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         if (navigationView != null) {
             navigationView.setNavigationItemSelectedListener(this);
